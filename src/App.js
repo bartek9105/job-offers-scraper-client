2d360d1c270feaecccd9f2e6d289db369130df15
bartek9105 <bartek9105@gmail.com>
2021-01-23 14:16:05 +0100

    Fetch technologies data, count each technology occurence

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+  const [technologies, setTechnologies] = useState([])
+  const [countedTechnologies, setCountedTechnologies] = useState({})
+
+  const fetchTechnologies = async () => {
+    try {
+      const technologies = await axios.get('http://localhost:5000/api/v1/offers')
+      setTechnologies(technologies.data)
+      const counter = {}
+      const techs = []
+      technologies.data.map(technology => {
+        technology.technologies.map(item => {
+          item.technologies.map(el => techs.push(el))
+        })
+      })
+      techs.forEach(el => {
+        counter[el] ? counter[el]++ : counter[el] = 1
+      })
+      setCountedTechnologies(counter)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  useEffect(() => {
+    fetchTechnologies()
+  }, [])
+
   return (
-    <div className='App'>
-      <h1>React App</h1>
-    </div>
+    <>
+      {Object.keys(countedTechnologies).map(key => (
+        <p>{key} {countedTechnologies[key]}</p>
+      ))}
+    </>
   )
 }
 
