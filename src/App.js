e6dcfa193253723db7ff736548af4c2199534c8b
bartek9105 <bartek9105@gmail.com>
2021-01-25 16:59:43 +0100

    Update fetch endpoint and logic, remove prime react library

--- src/App.js
+++ src/App.js
@@ -1,44 +1,30 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
-import Table from './Table'
 import './App.css'
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
+import Table from './Table'
 
 function App() {
-  const [offers, setOffers] = useState([])
-  const [technologiesCounter, setTechnologiesCounter] = useState({})
+  const [technologiesCount, setTechnologiesCount] = useState([])
 
-  const fetchTechnologies = async () => {
+  const fetchTechnologiesCount = async () => {
     try {
-      const technologies = await axios.get('http://localhost:5000/api/v1/offers')
-      setOffers(technologies.data)
-
-      const techs = []
-      const counter = {}
-
-      offers.map(offer => offer.offers.map(el => el.technologies.map(item => techs.push(item))))
-
-      techs.forEach(tech => {
-        counter[tech] ? counter[tech]++ : counter[tech] = 1
-      })
-
-      setTechnologiesCounter(Object.keys(counter).map(key => ({ technology: key, amount: counter[key] })))
-
+      const technologiesCount = await axios.get(
+        'http://localhost:5000/api/v1/offers/technologies'
+      )
+      setTechnologiesCount(technologiesCount.data)
     } catch (error) {
       console.error(error)
     }
   }
 
   useEffect(() => {
-    fetchTechnologies()
+    fetchTechnologiesCount()
   }, [])
 
   return (
-    <div className="app">
+    <div className='app'>
       <h1>Technologies popularity</h1>
-      <Table data={technologiesCounter} />
+      <Table data={technologiesCount} />
     </div>
   )
 }
--- src/Table.js
+++ src/Table.js
@@ -1,14 +1,8 @@
-import { DataTable } from 'primereact/datatable';
-import { Column } from 'primereact/column';
-
 const Table = ({ data }) => {
   return (
     <div>
       <div className="card">
-        <DataTable value={data} sortField="amount" scrollable scrollHeight="500px">
-          <Column field="technology" header="Technology" ></Column>
-          <Column field="amount" header="Offers amount" sortable></Column>
-        </DataTable>
+        Table
       </div>
     </div>
   );
